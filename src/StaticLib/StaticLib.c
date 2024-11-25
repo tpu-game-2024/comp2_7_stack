037b66c9fe8e0fd780638a1ea284eb6f833e901f
JcdbTPU40 <g2427040@st.t-kougei.ac.jp>
2024-11-25 15:14:54 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -10,48 +10,100 @@ void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
 
-	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	s->stack_memory = (int*)malloc(mem_size);
+	if (s->stack_memory == NULL)
+	{
+		s->stack_pointer = NULL;
+		s->end = NULL;
+	}
+	else
+	{
+		s->stack_pointer = s->stack_memory;
+		size_t num_elements = mem_size / sizeof(int);
+		s->end = s->stack_memory + num_elements;
+	}
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL) return;
+
+	if (s->stack_memory != NULL) {
+		free(s->stack_memory);
+		s->stack_memory = NULL;
+		s->stack_pointer = NULL;
+		s->end = NULL;
+	}
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_memory == NULL || s->stack_pointer == NULL || s->end == NULL)
+		return false;
+
+	if (s->stack_pointer >= s->end)
+		return false;
+
+	*(s->stack_pointer) = val;
+	s->stack_pointer++;
+
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_memory == NULL || s->stack_pointer == NULL || s->end == NULL)
+		return false;
+
+	if (addr == NULL || num <= 0)
+		return false;
+
+	if ((s->stack_pointer + num) > s->end)
+		return false;
+
+	for (int i = num - 1; i >= 0; i--) {
+		*(s->stack_pointer) = addr[i];
+		s->stack_pointer++;
+	}
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
-	// ToDo: スタックの最上位の値を取り出して返そう
-	// 不具合時は0を返す
-	return 0;
+	if (s == NULL || s->stack_memory == NULL || s->stack_pointer == NULL || s->end == NULL)
+		return 0;
+
+	if (s->stack_pointer == s->stack_memory)
+		return 0;
+
+	s->stack_pointer--;
+	return *(s->stack_pointer);
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
-	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
-	// 積んだ要素数を返り値として返そう
-	return 0;
+	if (s == NULL || s->stack_memory == NULL || s->stack_pointer == NULL || s->end == NULL)
+		return 0;
+
+	if (addr == NULL || num <= 0)
+		return 0;
+
+	int available = (int)(s->stack_pointer - s->stack_memory);
+	int count = (num <= available) ? num : available;
+
+	for (int i = 0; i < count; i++) {
+		s->stack_pointer--;
+		addr[i] = *(s->stack_pointer);
+	}
+
+	return count;
 }
