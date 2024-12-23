120fd02c3953ae15dc8e50d5f98d83b8e5333bc5
hashimoto0517 <izumi.gsmz1@gmail.com>
2024-12-23 17:13:13 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,11 +9,19 @@
 void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
-
 	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+
+	s->stack_memory = malloc(mem_size);
+	if (s->stack_memory == NULL) 
+	{
+		s->stack_pointer = NULL;
+		s->stack_memory = NULL;
+		s->end = NULL;
+		return;
+	}
+
+	s->stack_pointer = s->stack_memory + mem_size / sizeof(int);
+	s->end = s->stack_memory + mem_size / sizeof(int);
 }
 
 
@@ -21,6 +29,11 @@ void initialize(STACK* s, size_t mem_size)
 void finalize(STACK* s)
 {
 	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s != NULL) 
+	{
+		free(s->stack_memory);
+		s->stack_memory = NULL;
+	}
 }
 
 
@@ -28,7 +41,17 @@ void finalize(STACK* s)
 bool push(STACK* s, int val)
 {
 	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL || s->stack_pointer == NULL)
+		return false;
+
+	if (s->stack_pointer > s->stack_memory)
+		s->stack_pointer--;
+	else
+		return false;
+
+	*(s->stack_pointer) = val;
+	return true;
+
 }
 
 
@@ -36,7 +59,19 @@ bool push(STACK* s, int val)
 bool push_array(STACK* s, int* addr, int num)
 {
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL || addr == NULL || num <= 0)
+		return false;
+
+	if (s->stack_pointer - num < s->stack_memory)
+		return false;
+
+	for (int i = 0; i < num; i++) 
+	{
+		if (!push(s, addr[(num - 1) - i])) 
+			return false;
+		
+	}
+	return true;
 }
 
 // スタックから一つの要素を取り出す
@@ -44,7 +79,16 @@ int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+
+	if (s == NULL) 
+		return 0;
+	int n = *s->stack_pointer;
+	if (s->stack_pointer != s->end) 
+		s->stack_pointer++;
+	else
+		return 0;
+
+	return n;
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -53,5 +97,18 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+
+	if (s == NULL) return 0;
+	if (addr == NULL) return 0;
+	if (num <= 0) return 0;
+	if (s->stack_pointer == s->end) {
+		return 0;
+	}
+	for (int i = 0; i < num; i++) {
+		addr[i] = pop(s);
+		if (addr[i] == 0) {
+			return i;
+		}
+	}
+	return num;
 }
