048065d812b07e910a888a33df9d8fb01c98c6f6
MankiYusuke <takosumi1271@ymail.ne.jp>
2024-12-28 05:03:20 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -8,43 +8,78 @@
 // mem_size の容量でスタック用のメモリを確保する
 void initialize(STACK* s, size_t mem_size)
 {
-	if (s == NULL) return;
-
 	// ToDo: mem_sizeでメモリを確保しよう
-	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
-	s->end = NULL;
+	if (s == NULL) return;
+	
+	s->stack_memory =  (int*)malloc(mem_size);
+	if (s->stack_memory == NULL)
+	{
+		s->stack_pointer = NULL;
+		s->end = NULL;
+		return;
+	}
+	s->end = &s->stack_memory[mem_size / sizeof(int)];
+	s->stack_pointer = s->end;
 }
 
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
-	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL)return;
+	free(s->stack_memory);
+	s->stack_memory = NULL;
+	s->stack_pointer = NULL;
+	s->end = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
-	// ToDo: valの値をスタックに保存しよう
-	return false;
+	if (s == NULL)return false;
+	if (s->stack_pointer < s->stack_memory + 1) {
+		return false;
+	}
+	else {
+		s->stack_pointer--;
+		*s->stack_pointer = val;
+		return true;
+	}
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
-	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	if (s == NULL)return false;
+
+	if (addr == NULL || num <= 0) return false;
+
+	if (s->stack_memory < num)    return false;
+
+	if (s->stack_pointer < s->stack_memory + num) return false;
+	
+	for (int i = num - 1; i >= 0; i--)
+	{
+		push(s, addr[i]);
+	}
+
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
-	// 不具合時は0を返す
-	return 0;
+	if (s == NULL) return 0;
+
+	if (s->end < s->stack_pointer + 1)return 0;
+
+	
+		int num = *s->stack_pointer;
+		s->stack_pointer++;
+		return num;
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -53,5 +88,20 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+
+	if (s == NULL || s->stack_memory == NULL || s->stack_pointer == NULL || s->end == NULL)
+		return 0;
+
+	if (addr == NULL || num <= 0) return 0;
+
+	if (s->stack_pointer == s->end) return 0;
+	
+
+	int attempts = 1;
+	for (int i = 0; i < num; i++) {
+		
+		addr[i] = pop(s);
+		if (addr[i] == 0) return i;
+	}
+	return num;
 }
